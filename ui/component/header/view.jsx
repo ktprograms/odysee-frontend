ffe232ebf1e52be5690b9d3c6d19d99d2dcc86bf
infinite-persistence <inf.persistence@gmail.com>
2022-01-04 20:28:16 +0800

    Keycloak - refactor login button for re-use

--- ui/component/header/view.jsx
+++ ui/component/header/view.jsx
@@ -4,7 +4,6 @@ import 'scss/component/_header.scss';
 import { formatCredits } from 'util/format-credits';
 import { useIsMobile } from 'effects/use-screensize';
 import { withRouter } from 'react-router';
-import { useKeycloak } from '@react-keycloak/web';
 import * as ICONS from 'constants/icons';
 import * as PAGES from 'constants/pages';
 import Button from 'component/button';
@@ -17,6 +16,7 @@ import React from 'react';
 import Skeleton from '@mui/material/Skeleton';
 import SkipNavigationButton from 'component/skipNavigationButton';
 import Tooltip from 'component/common/tooltip';
+import UserOAuthButton from 'component/userOAuthButton';
 import WunderBar from 'component/wunderbar';
 
 type Props = {
@@ -43,7 +43,6 @@ type Props = {
   sidebarOpen: boolean,
   syncError: ?string,
   totalBalance?: number,
-  user: ?User,
   prefsReady: boolean,
   doClearClaimSearch: () => void,
   clearEmailEntry: () => void,
@@ -67,7 +66,6 @@ const Header = (props: Props) => {
     sidebarOpen,
     syncError,
     totalBalance,
-    user,
     prefsReady,
     doClearClaimSearch,
     clearEmailEntry,
@@ -84,7 +82,6 @@ const Header = (props: Props) => {
   } = history;
 
   const isMobile = useIsMobile();
-  const { initialized: keycloakReady } = useKeycloak();
 
   // on the verify page don't let anyone escape other than by closing the tab to keep session data consistent
   const isVerifyPage = pathname.includes(PAGES.AUTH_VERIFY);
@@ -171,11 +168,7 @@ const Header = (props: Props) => {
         </>
       ) : !isMobile ? (
         <div className="header__authButtons">
-          {!keycloakReady || user === undefined ? (
-            <Skeleton variant="text" animation="wave" className="header__navigationItem--balanceLoading" />
-          ) : (
-            <Button navigate={`/$/${PAGES.OAUTH}`} button="primary" label={__('Log In')} disabled={user === null} />
-          )}
+          <UserOAuthButton />
         </div>
       ) : (
         <HeaderProfileMenuButton />
